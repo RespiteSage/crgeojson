eabf809db4243991b91003f4e35641b93ad2f5e3
Benjamin Wade <benjamin@va.wagner.com>
2019-03-05 14:14:06 -0500

    Create spec scaffold for CoordinateTree

---
+++ spec/coordinate_tree_spec.cr
@@ -0,0 +1,61 @@
+require "./spec_helper"
+
+describe CoordinateTree do
+  describe Root do
+    describe ".new" do
+      # TODO
+    end
+
+    describe "#leaf_value" do
+      # TODO
+    end
+
+    describe "#parent" do
+      # TODO
+    end
+
+    describe "#children" do
+      # TODO
+    end
+  end
+
+  describe Branch do
+    describe ".new" do
+      # TODO
+    end
+
+    describe "#leaf_value" do
+      # TODO
+    end
+
+    describe "#parent" do
+      # TODO
+    end
+
+    describe "#children" do
+      # TODO
+    end
+  end
+
+  describe Leaf do
+    describe ".new" do
+      # TODO
+    end
+
+    describe "#leaf_value" do
+      # TODO
+    end
+
+    describe "#parent" do
+      # TODO
+    end
+
+    describe "#children" do
+      # TODO
+    end
+  end
+
+  describe ".from_geojson" do
+    # TODO
+  end
+end
